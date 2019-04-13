2b5a77aae829f1fdb09689558a3b9db1087b64b6
Seiji Emery <seijiemery@gmail.com>
2019-04-12 22:52:18 -0700

    added comments

--- cpp/src/arith.cpp
+++ cpp/src/arith.cpp
@@ -1,14 +1,23 @@
 #include <iostream>
 #include <cstdio>
 
+// Tags for traversing the arith AST
 enum { TYPE_CONST = 0, TYPE_ADD, TYPE_MUL, TYPE_IF };
 
-template <int x>
+// AST elements are c++ types.
+// I am -sort of- cheating by directly / eagerly evaluating the AST's result,
+// but this is the only way to do this with templates. FWIW, it's still fully
+// possible to walk the AST after the fact since all AST nodes have references
+// stored to them (see Left, Right, etc).
+
+template <int x>                // argument (a 32-bit integer)
 struct Const {
     enum { 
-        type = TYPE_CONST, 
-        result = x 
+        type = TYPE_CONST,      // type tag. useful only for traversal; unused here
+        result = x              // store value (technically, we're eagerly evaluating our results)
     };
+
+    // stringification, implemented for both std::ostream and C's printf()
     static std::ostream& repr (std::ostream& os) {
         return os << "Const<" << x << ">";
     }
@@ -19,11 +28,11 @@ struct Const {
 
 template <typename L, typename R>
 struct Add {
-    typedef L Left;
-    typedef R Right;
+    typedef L Left;     // store results. Accessible as (this-type)::Left, (this-type)::Right.
+    typedef R Right;    // these are assumed to be other Arith AST nodes, and can be of any type.
     enum { 
-        type = TYPE_ADD,
-        result = Left::result + Right::result
+        type = TYPE_ADD,                        // type tag
+        result = Left::result + Right::result   // again, results are eagerly evaluated
     };
     static std::ostream& repr (std::ostream& os) {
         return os << "Add<",
@@ -53,20 +62,30 @@ struct Mul {
     }
 };
 
+// New feature: ternary If.
+// usage:
+//   If< Cond1, Cond2, Lesser, Zero, Greater >
+//
+// Evaluates Cond1 and Cond2, then:
+//  if Cond1 < Cond2, evaluates and returns Lesser
+//  if Cond1 = Cond2, evaluates and returns Zero
+//  if Cond1 > Cond2, evaluates and returns Greater
+//
+// where Cond1, Cond2, Lesser, Zero, and Greater are all Arith expressions (AST nodes).
+
+// Helper: implement ternary selection / branching
 namespace detail {
+    template <bool z, bool g, typename L, typename Z, typename G>
+    struct SelectTernary;
 
-template <bool z, bool g, typename L, typename Z, typename G>
-struct SelectTernary;
+    template <bool any, typename L, typename Z, typename G>
+    struct SelectTernary<true, any, L, Z, G> { typedef Z Result; };
 
-template <bool any, typename L, typename Z, typename G>
-struct SelectTernary<true, any, L, Z, G> { typedef Z Result; };
+    template <typename L, typename Z, typename G>
+    struct SelectTernary<false, true, L, Z, G> { typedef G Result; };
 
-template <typename L, typename Z, typename G>
-struct SelectTernary<false, true, L, Z, G> { typedef G Result; };
-
-template <typename L, typename Z, typename G>
-struct SelectTernary<false, false, L, Z, G> { typedef L Result; };
-
+    template <typename L, typename Z, typename G>
+    struct SelectTernary<false, false, L, Z, G> { typedef L Result; };
 }; // namespace detail
 
 template <typename CL, typename CR, typename L, typename Z, typename G>
@@ -104,9 +123,27 @@ struct If {
     }
 };
 
+// Macros for evaluation and stringification / printing.
+// Again, I'm sort of "cheating" by eagerly evaluating AST nodes as they are constructed,
+// though it is concievable that a language implementation could do this.
+
+// evaluates an expression, returning its evaluated result
 #define eval(Expr) (Expr::result)
+
+// prints an expression to std::cout using Expr::repr
 #define repr(Expr) (Expr::repr(std::cout))
+
+// prints an expression to standard output using Expr::printf
 #define print(Expr) (Expr::printf())
+
+// printEval prints an expression followed by what it evaluates to.
+//
+// The implementation can be changed via compiler flags:
+//  -DNO_OUTPUT,
+//  -DUSE_IOSTREAM, or
+//  -DUSE_PRINTF
+//
+// By default it will use printf.
 
 #if defined(NO_OUTPUT)
     #define printEval(Expr)
@@ -116,10 +153,21 @@ struct If {
     #define printEval(Expr) (print(Expr), printf(" => %d\n", eval(Expr)))
 #endif
 
+// asserts that an expression evaluates to result, checked at compile time using
+// static_assert. Will print a somewhat helpful error message if it fails.
+//
+// (If you are doing assertEvaluatesTo(Mul<Add<Const<2>, Const<3>>, Const<4>>, 21),
+//  you will get a somewhat helpful message that this does not evaluate to 21.
+//  If you aliased the type as using Foo = ...; assertEvaluatesTo(Foo, 21), this
+//  message is... somewhat less helpful)
+//
 #define assertEvaluatesTo(Expr, result) \
     static_assert(eval(Expr) == result, #Expr " !=> " #result)
 
 int main () {
+    // Testcases. The If testcase is somewhat long yet basic, but obviously 
+    // could be combined with any of the other expressions here.
+
     using A = Const<12>;
     assertEvaluatesTo(A, 12);
     printEval(A);
